5a3f08ff0802b4f66f667374f527c4aa4f528af0
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2022-11-25 16:08:47 +0700

    menambahkan class node

--- Exercise3_003/Program.cs
+++ Exercise3_003/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Exercise3_003
+{
+    class Node
+    {
+        //membuat node untuk circular
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+    class CircularList
+    {
+        Node last;
+    }
+    public bool search
+}
